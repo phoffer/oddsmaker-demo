8c054d7fa9dd888a144e46a8ad07dabf1914a921
Paul Hoffer <git@paulhoffer.com>
2017-10-08 18:26:38 -0600

    remove extra stuff

--- app.rb
+++ app.rb
@@ -1,6 +1,5 @@
 require 'roda'
 require 'oddsmaker'
-# require 'pry'
 
 class App < Roda
   plugin :assets, css: ['bulma.min.css', 'app.css'], js: ['jquery-3.2.1.min.js', 'app.js']
@@ -12,12 +11,11 @@ class App < Roda
   end
 
   route do |r|
-    r.assets# unless ENV['RACK_ENV'] == 'production'
+    r.assets
     r.root do
       render 'index.html'
     end
     r.get 'odds' do
-      puts r.params.inspect
       odds = r.params['odds'].values.map { |hash| Oddsmaker::Odd.new(hash) }
       market = Oddsmaker::Market.new(odds, name: r.params['name'])
       market.to_json
